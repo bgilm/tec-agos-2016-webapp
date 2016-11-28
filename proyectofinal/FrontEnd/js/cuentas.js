129f9388ba906ba7c2fbad98fd50cf8f5682e1cd
bgilm <b.gil@me.com>
2016-11-28 14:53:36 -0600

    angular en el post de cuentas egreso e ingresos

---
+++ proyectofinal/FrontEnd/js/cuentas.js
@@ -0,0 +1,53 @@
+         var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+        var encodedString = Base64.encode('1:4hc-3cd13925d758704bffe4');
+        //var username= document.getElementById("username").value;
+        console.log(encodedString);
+        var app = angular.module("prueba", []);
+
+        app.controller('MainCtrl', function ($scope,$http) {
+            console.log($scope);
+            $scope.altaCuenta = function(){
+//                var banco=document.getElementById("banco").value;
+//                
+//                var numero_cuenta=document.getElementById("numero_cuenta").value;
+//                var clabe=document.getElementById("clabe").value;
+//                var tipo_cuenta=document.getElementById("tipo_cuenta").value;
+//                var monto=document.getElementById("monto").value;
+//                var t_tarjeta_debito =document.getElementById("t_tarjeta_debito").value;
+//                var num_tarjeta=document.getElementById("num_tarjeta").value;
+//                    var tasa_inflacion=document.getElementById("tasa_inflacion").value;
+//                var plazo =document.getElementById("plazo").value;   
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:8000/perfil/cuenta/agregar',
+              data:{username:'admin',
+                          banco:$scope.banco,
+                          numero_cuenta: $scope.numero_cuenta,
+                          clabe:$scope.clabe,
+                          tipo_cuenta:$scope.tipo_cuenta,
+                          monto:$scope.monto,
+                          t_tarjeta_debito:$scope.t_tarjeta_debito,
+                          num_tarjeta:$scope.num_tarjeta,
+                          tasa_inflacion:$scope.tasa_inflacion,
+                          plazo:$scope.plazo,
+                    },
+           
+            headers: {
+                'Authorization':'Basic '+encodedString
+            }
+            
+            }
+             $http(req).then(function successCallback(response) {
+                        console.log("Success"+response);
+                  }, function errorCallback(response) {
+                        console.log($scope.tipo_cuenta);
+                        console.log($scope.t_tarjeta_debito);
+                        console.log("Error"+response);
+            });
+            }
+        });
+
+       
+
+    
---
+++ proyectofinal/FrontEnd/js/ingresos.js
@@ -0,0 +1,63 @@
+         var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+        var encodedString = Base64.encode('1:4hc-3cd13925d758704bffe4');
+        //var username= document.getElementById("username").value;
+        
+            
+       
+        console.log(encodedString); // Outputs: "SGVsbG8gV29ybGQh"
+        var app = angular.module("prueba", []);
+        app.controller('MainCtrl', function ($scope,$http) {
+//            $http({ method: 'POST', 
+//                    url: 'http://localhost:8000/cuenta/Registrar',
+//                    headers:{
+//                      Authorization:'Basic '+encodedString,
+//                    },
+//                    data:{username:'admin',
+//                          banco:'HSBC',
+//                          numero_cuenta: '123',
+//                          clabe:'123',
+//                          tipo_cuenta:'Cuenta de ahorros',
+//                          monto:'123',
+//                          t_tarjeta_debito:'True',
+//                          num_tarjeta:'123',
+//                          tasa_inflacion:'123',
+//                          plazo:'123',
+//                    }
+//                  })
+            $scope.altaCuenta = function(){
+                var cuenta=document.getElementById("cuenta").value;
+                var nombre=document.getElementById("nombre").value;
+                var monto=document.getElementById("monto").value;
+                var recurrencia=document.getElementById("recurrencia").value;
+                var fijo=document.getElementById("fijo").value;
+                var tipo =document.getElementById("tipo").value;
+                var fecha =document.getElementById("fecha").value;
+                
+              
+                
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:8000/perfil/ingreso/agregar',
+              data:{username:'admin',
+                          tarjeta:tarjeta,
+                          nombre: nombre,
+                          monto:monto,
+                          recurrencia:recurrencia,
+                          fijo:fijo,
+                          tipo:tipo,
+                          fecha:fecha   
+                    },
+           
+            headers: {
+                'Authorization':'Basic '+encodedString
+            }
+          
+            }
+             $http(req).then(function successCallback(response) {
+                        console.log("Success"+response)
+                  }, function errorCallback(response) {
+                        console.log("Error"+response)
+            });
+            }
+        });
